1c3233de1d31e4fe8a1ae0ec9e1c624114c70201
bangdeangee <bangdeangee@gmail.com>
2020-01-21 14:12:48 +0900

    little modify /blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -15,10 +15,6 @@ def post_detail(request, pk): #PK 매개변수 추가
     post = get_object_or_404(Post, pk=pk) #exception
     return render(request, 'blog/post_detail.html', {'post': post})
 
-#def post_new(request):
-#    form = PostForm()
-#    return render(request, 'blog/post_edit.html', {'form': form})
-
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
